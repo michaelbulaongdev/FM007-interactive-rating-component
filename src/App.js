54eb7e36ac1a677e82f09e8d8d65f46f73fd0a5a
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-03 03:35:31 +0800

    layout for rating card

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import {useState} from 'react';
 import {Container} from '@mui/material';
 import ThankYou from './components/ThankYou';
-import Attribution from './components/Attribution';
+// import Attribution from './components/Attribution';
+import Rating from './components/Rating';
 
 export default function App() {
-	const [submitted, setSubmitted] = useState(true);
+	const [submitted, setSubmitted] = useState(false);
 	const [rating, setRating] = useState('0');
 
 	const handleClick = (e) => {
@@ -20,19 +21,18 @@ export default function App() {
 		<Container
 			maxWidth='sm'
 			sx={{
-				textAlign: 'center',
 				alignContent: 'stretch',
 			}}>
 			{submitted ? (
-				<div className='card-container'>
+				<div className='thankyou card-container'>
 					<ThankYou rating={rating} />
 				</div>
 			) : (
 				<div className='card-container'>
-					<h1>How did we do?</h1>
+					<Rating handleClick={handleClick} handleSubmit={handleSubmit} />
 				</div>
 			)}
-			<Attribution />
+			{/* <Attribution /> */}
 		</Container>
 	);
 }
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,8 +1,26 @@
-How did we do?
+import {ReactComponent as StarLogo} from '../images/icon-star.svg';
 
-  Please let us know how we did with your support request. All feedback is appreciated 
-  to help us improve our offering!
+export default function Rating(props) {
+	const numberButtons = [1, 2, 3, 4, 5];
 
-  1 2 3 4 5
-
-  Submit
+	return (
+		<div>
+			<div className='star-circle'>
+				<StarLogo />
+			</div>
+			<div>
+				<h1>How did we do?</h1>
+				<p>
+					Please let us know how we did with your support request. All feedback
+					is appreciated to help us improve our offering!
+				</p>
+			</div>
+			<div className='numbers'>
+				<button className='button'>1</button>
+			</div>
+			<div className='submit-button'>
+				<button onSubmit={props.handleSubmit}>Submit</button>
+			</div>
+		</div>
+	);
+}
